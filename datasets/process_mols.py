addd0e9756e54c1631f9a5e078c2c7efdae69bf7
Gabriele Corso <gabri.corso@gmail.com>
2022-10-06 19:00:20 -0400

    avoid failure in case of unreadable molecules

--- datasets/process_mols.py
+++ datasets/process_mols.py
@@ -520,7 +520,8 @@ def read_molecule(molecule_file, sanitize=False, calc_charges=False, remove_hs=F
             mol = Chem.RemoveHs(mol, sanitize=sanitize)
     except Exception as e:
         print(e)
-        exit("could not process mol")
+        print("could not process mol")
+        return None
 
     return mol
 
