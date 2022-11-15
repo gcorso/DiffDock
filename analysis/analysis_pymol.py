17ebeaae551684f3bdb47f3bb8a3306f479eb16f
yaoyy.hi <>
2022-11-15 15:09:47 +0800

    color ligand w/ confidence score

--- analysis/analysis_pymol.py
+++ analysis/analysis_pymol.py
@@ -2,9 +2,9 @@
 # Visualize DiffDock results
 
 import pathlib,os
-from pymol import cmd
+from pymol import cmd,util
 import argparse
-
+from matplotlib.cm import get_cmap
 
 def parse_args():
   parser = argparse.ArgumentParser(description='Visualize DiffDock results with PyMOL')
@@ -13,6 +13,7 @@ def parse_args():
   parser.add_argument('--ligand_name', action="store", help='Input Ligand Name',type=str, required=True)
   parser.add_argument('--rank', action="store",  nargs=2,default= [1, 20], help="Show number of rank")
   parser.add_argument('--suffix', action="store", default='diffdock', help="Show number of rank")
+  parser.add_argument('--cmap', action="store", default='bwr_r',type=str, help="Colormap for ligand")
   return parser.parse_args()
 
 
@@ -33,10 +34,39 @@ if __name__ == '__main__':
 
   LIGAND_FP=pathlib.Path(LIGAND_FP).resolve()
 
+  cmap = get_cmap(args.cmap)
+  def ligand_rank_color(cmap,data_list):
+    num_color=cmap.N
+    data_min,data_max=min(data_list),max(data_list)
+
+    # reflect to a scaled data list
+    colors=[list(cmap(int(num_color * ((item-data_min)/(data_max-data_min)))))[:3] for item in data_list]
+
+    return colors
+
+
+
+  # confidences
+
+  sdf_confidence=[x for x in os.listdir(f'{LIGAND_FP}') if '_confidence' in x and x.endswith('.sdf') and x.startswith('rank')]
+  num_predicts = len(sdf_confidence)
+  print(f'{num_predicts}: {sdf_confidence}')
+
+  # parse confidence score from sdf filename
+  confidence_dict={}
+  for rank in range(1,num_predicts+1):
+    sdf_rank=[x for x in sdf_confidence if x.startswith(f'rank{rank}_confidence')][0]
+    confidence_score=float(sdf_rank.split('confidence')[1].split('.sdf')[0])
+    print(f'reading confidence {confidence_score} from rank {rank} file: {sdf_rank}')
+    confidence_dict[rank]=confidence_score
+
+  # fetch color list
+  rank_color = ligand_rank_color(cmap=cmap, data_list=confidence_dict.values())
+
   cmd.load(filename=PROTEIN_FP,object=PROTEIN_NAME)
 
   for i in range(int(RANK_NUM[0]),int(RANK_NUM[1])+1):
-    try:
+    if os.path.exists(f'{LIGAND_FP}/rank{i}_reverseprocess.pdb'):
       # cmd.load( filename [,object [,state [,format [,finish [,discrete [,multiplex ]]]]]] )
       cmd.load(filename=f'{LIGAND_FP}/rank{i}_reverseprocess.pdb',
                object=f'{LIGAND_NAME}_{i}',
@@ -45,8 +75,14 @@ if __name__ == '__main__':
                multiplex=0,
                )
       cmd.do(f'h_fix {LIGAND_NAME}_{i}')
-    except:
-      print(f'error occurs while loading prediction #{i}.')
+      print(f'color rank#{i} with color {rank_color[i-1]}')
+      cmd.set_color(f'color_ranked_{i}', rank_color[i-1])
+      cmd.color(f'color_ranked_{i}', f'({LIGAND_NAME}_{i})')
+      util.cnc(f'({LIGAND_NAME}_{i})', _self=cmd)
+
+    else:
+      print(f'file not found: {LIGAND_FP}/rank{i}_reverseprocess.pdb')
+
 
   cmd.set('surface_cavity_mode',4)
   cmd.set('transparency',0.5)
@@ -54,7 +90,7 @@ if __name__ == '__main__':
   cmd.show('surface',PROTEIN_NAME)
   cmd.hide('cartoon')
 
-  cmd.set('stick_color','lime',f'{LIGAND_NAME}_*')
+
   cmd.set('stick_color','firebrick',PROTEIN_NAME)
 
 
