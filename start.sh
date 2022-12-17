58d3d89e5163bdc33e60b1e6df7576392423210f
Niklas Rindtorff <NiklasTR@users.noreply.github.com>
2022-12-17 20:29:32 +0100

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "pod started"
+
+if [[ $PUBLIC_KEY ]]
+then
+    mkdir -p ~/.ssh
+    chmod 700 ~/.ssh
+    cd ~/.ssh
+    echo $PUBLIC_KEY >> authorized_keys
+    chmod 700 -R ~/.ssh
+    cd /
+    service ssh start
+fi
+
+if [[ $JUPYTER_PASSWORD ]]
+then
+    cd /
+    jupyter lab --allow-root --no-browser --port=8888 --ip=* --ServerApp.terminado_settings='{"shell_command":["/bin/bash"]}' --ServerApp.token=$JUPYTER_PASSWORD --ServerApp.allow_origin=* --ServerApp.preferred_dir=/workspace
+else
+    sleep infinity
+fi
